e1ae0373d3853e9107c3bce647d8d63d658057c0
Vincent QUATREVIEUX <vquatrevieux@b2pweb.com>
2023-01-06 09:47:11 +0100

    fix: compatibility with prime 1 interfaces

--- src/AnalyzerCompilerAdapter.php
+++ src/AnalyzerCompilerAdapter.php
@@ -3,7 +3,6 @@
 namespace Bdf\Prime\Analyzer;
 
 use BadMethodCallException;
-use Bdf\Prime\Exception\PrimeException;
 use Bdf\Prime\Platform\PlatformInterface;
 use Bdf\Prime\Query\CompilableClause;
 use Bdf\Prime\Query\Compiler\CompilerInterface;
@@ -13,156 +12,276 @@ use Bdf\Prime\Query\Compiler\QuoteCompilerInterface;
 use Bdf\Prime\Query\Compiler\SelectCompilerInterface;
 use Bdf\Prime\Query\Compiler\UpdateCompilerInterface;
 
-/**
- * Adapt for CompilerInterface for perform query analysis
- *
- * @template Q as CompilableClause&\Bdf\Prime\Query\Contract\Compilable
- * @implements CompilerInterface<Q>
- */
-final class AnalyzerCompilerAdapter implements CompilerInterface, QuoteCompilerInterface
-{
+if (interface_exists(QuoteCompilerInterface::class)) {
     /**
-     * @var AnalyzerService
-     */
-    private $service;
-
-    /**
-     * @var object
-     */
-    private $compiler;
-
-    /**
-     * @var AnalyzerInterface
-     */
-    private $analyzer;
-
-    /**
-     * AnalyzerCompilerAdapter constructor.
+     * Adapt for CompilerInterface for perform query analysis
      *
-     * @param AnalyzerService $service
-     * @param object $compiler
-     * @param AnalyzerInterface $analyzer
+     * @template Q as CompilableClause&\Bdf\Prime\Query\Contract\Compilable
+     * @implements CompilerInterface<Q>
      */
-    public function __construct(AnalyzerService $service, $compiler, AnalyzerInterface $analyzer)
+    final class AnalyzerCompilerAdapter implements CompilerInterface, QuoteCompilerInterface
     {
-        $this->service = $service;
-        $this->compiler = $compiler;
-        $this->analyzer = $analyzer;
-    }
+        /**
+         * @var AnalyzerService
+         */
+        private $service;
 
-    /**
-     * {@inheritdoc}
-     */
-    public function compileInsert(CompilableClause $query)
-    {
-        $compiler = $this->compiler;
+        /**
+         * @var object
+         */
+        private $compiler;
 
-        if (!$compiler instanceof InsertCompilerInterface) {
-            throw new BadMethodCallException();
+        /**
+         * @var AnalyzerInterface
+         */
+        private $analyzer;
+
+        /**
+         * AnalyzerCompilerAdapter constructor.
+         *
+         * @param AnalyzerService $service
+         * @param object $compiler
+         * @param AnalyzerInterface $analyzer
+         */
+        public function __construct(AnalyzerService $service, $compiler, AnalyzerInterface $analyzer)
+        {
+            $this->service = $service;
+            $this->compiler = $compiler;
+            $this->analyzer = $analyzer;
         }
 
-        $this->analyze($query);
+        /**
+         * {@inheritdoc}
+         */
+        public function compileInsert(CompilableClause $query)
+        {
+            $compiler = $this->compiler;
 
-        return $this->compiler->compileInsert($query);
-    }
+            if (!$compiler instanceof InsertCompilerInterface) {
+                throw new BadMethodCallException();
+            }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function compileUpdate(CompilableClause $query)
-    {
-        $compiler = $this->compiler;
+            $this->analyze($query);
 
-        if (!$compiler instanceof UpdateCompilerInterface) {
-            throw new BadMethodCallException();
+            return $compiler->compileInsert($query);
         }
 
-        $this->analyze($query);
+        /**
+         * {@inheritdoc}
+         */
+        public function compileUpdate(CompilableClause $query)
+        {
+            $compiler = $this->compiler;
 
-        return $this->compiler->compileUpdate($query);
-    }
+            if (!$compiler instanceof UpdateCompilerInterface) {
+                throw new BadMethodCallException();
+            }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function compileDelete(CompilableClause $query)
-    {
-        $compiler = $this->compiler;
+            $this->analyze($query);
 
-        if (!$compiler instanceof DeleteCompilerInterface) {
-            throw new BadMethodCallException();
+            return $compiler->compileUpdate($query);
         }
 
-        $this->analyze($query);
+        /**
+         * {@inheritdoc}
+         */
+        public function compileDelete(CompilableClause $query)
+        {
+            $compiler = $this->compiler;
 
-        return $this->compiler->compileDelete($query);
-    }
+            if (!$compiler instanceof DeleteCompilerInterface) {
+                throw new BadMethodCallException();
+            }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function compileSelect(CompilableClause $query)
-    {
-        $compiler = $this->compiler;
+            $this->analyze($query);
 
-        if (!$compiler instanceof SelectCompilerInterface) {
-            throw new BadMethodCallException();
+            return $compiler->compileDelete($query);
         }
 
-        $this->analyze($query);
+        /**
+         * {@inheritdoc}
+         */
+        public function compileSelect(CompilableClause $query)
+        {
+            $compiler = $this->compiler;
 
-        return $this->compiler->compileSelect($query);
-    }
+            if (!$compiler instanceof SelectCompilerInterface) {
+                throw new BadMethodCallException();
+            }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function platform(): PlatformInterface
-    {
-        return $this->compiler->platform();
-    }
+            $this->analyze($query);
 
-    /**
-     * {@inheritdoc}
-     */
-    public function quoteIdentifier(CompilableClause $query, $column): string
-    {
-        $compiler = $this->compiler;
-
-        if (!$compiler instanceof QuoteCompilerInterface) {
-            throw new BadMethodCallException();
+            return $compiler->compileSelect($query);
         }
 
-        return $compiler->quoteIdentifier($query, $column);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function quote($value)
-    {
-        $compiler = $this->compiler;
-
-        if (!$compiler instanceof QuoteCompilerInterface) {
-            throw new BadMethodCallException();
+        /**
+         * {@inheritdoc}
+         */
+        public function platform(): PlatformInterface
+        {
+            return $this->compiler->platform();
         }
 
-        return $compiler->quote($value);
-    }
+        /**
+         * {@inheritdoc}
+         */
+        public function quoteIdentifier(CompilableClause $query, $column): string
+        {
+            $compiler = $this->compiler;
 
+            if (!$compiler instanceof QuoteCompilerInterface) {
+                throw new BadMethodCallException();
+            }
+
+            return $compiler->quoteIdentifier($query, $column);
+        }
+
+        /**
+         * {@inheritdoc}
+         */
+        public function quote($value)
+        {
+            $compiler = $this->compiler;
+
+            if (!$compiler instanceof QuoteCompilerInterface) {
+                throw new BadMethodCallException();
+            }
+
+            return $compiler->quote($value);
+        }
+
+        /**
+         * {@inheritdoc}
+         */
+        public function getBindings(CompilableClause $query): array
+        {
+            return $this->compiler->getBindings($query);
+        }
+
+        private function analyze(CompilableClause $query): void
+        {
+            if ($report = $this->service->createReport($this->analyzer->entity($query))) {
+                $this->analyzer->analyze($report, $query);
+                $this->service->push($report);
+            }
+        }
+    }
+} else  {
     /**
-     * {@inheritdoc}
+     * Adapt for CompilerInterface for perform query analysis
+     *
+     * @template Q as CompilableClause&\Bdf\Prime\Query\Contract\Compilable
+     * @implements CompilerInterface<Q>
      */
-    public function getBindings(CompilableClause $query): array
+    final class AnalyzerCompilerAdapter implements CompilerInterface
     {
-        return $this->compiler->getBindings($query);
-    }
+        /**
+         * @var AnalyzerService
+         */
+        private $service;
 
-    private function analyze(CompilableClause $query): void
-    {
-        if ($report = $this->service->createReport($this->analyzer->entity($query))) {
-            $this->analyzer->analyze($report, $query);
-            $this->service->push($report);
+        /**
+         * @var CompilerInterface<Q>
+         */
+        private $compiler;
+
+        /**
+         * @var AnalyzerInterface
+         */
+        private $analyzer;
+
+        /**
+         * AnalyzerCompilerAdapter constructor.
+         *
+         * @param AnalyzerService $service
+         * @param CompilerInterface<Q> $compiler
+         * @param AnalyzerInterface $analyzer
+         */
+        public function __construct(AnalyzerService $service, $compiler, AnalyzerInterface $analyzer)
+        {
+            $this->service = $service;
+            $this->compiler = $compiler;
+            $this->analyzer = $analyzer;
+        }
+
+        /**
+         * {@inheritdoc}
+         */
+        public function compileInsert(CompilableClause $query)
+        {
+            $this->analyze($query);
+
+            return $this->compiler->compileInsert($query);
+        }
+
+        /**
+         * {@inheritdoc}
+         */
+        public function compileUpdate(CompilableClause $query)
+        {
+            $this->analyze($query);
+
+            return $this->compiler->compileUpdate($query);
+        }
+
+        /**
+         * {@inheritdoc}
+         */
+        public function compileDelete(CompilableClause $query)
+        {
+            $this->analyze($query);
+
+            return $this->compiler->compileDelete($query);
+        }
+
+        /**
+         * {@inheritdoc}
+         */
+        public function compileSelect(CompilableClause $query)
+        {
+            $this->analyze($query);
+
+            return $this->compiler->compileSelect($query);
+        }
+
+        /**
+         * {@inheritdoc}
+         */
+        public function platform(): PlatformInterface
+        {
+            return $this->compiler->platform();
+        }
+
+        /**
+         * {@inheritdoc}
+         */
+        public function quoteIdentifier(CompilableClause $query, $column): string
+        {
+            return $this->compiler->quoteIdentifier($query, $column);
+        }
+
+        /**
+         * {@inheritdoc}
+         */
+        public function quote($value)
+        {
+            return $this->compiler->quote($value);
+        }
+
+        /**
+         * {@inheritdoc}
+         */
+        public function getBindings(CompilableClause $query): array
+        {
+            return $this->compiler->getBindings($query);
+        }
+
+        private function analyze(CompilableClause $query): void
+        {
+            if ($report = $this->service->createReport($this->analyzer->entity($query))) {
+                $this->analyzer->analyze($report, $query);
+                $this->service->push($report);
+            }
         }
     }
 }
